81c8b0a3fc233e4c1c26e8b2a10a14163d3af82f
Spade <spade.dev@gmail.com>
2015-01-30 10:06:23 -0500

    Comments to clarify each function
    Adding comments to a boss fight, and a gossip menu.

--- Hellfire Ramparts/Boss1.lua
+++ Hellfire Ramparts/Boss1.lua
@@ -1,5 +1,6 @@
 local npcId = 600022
 
+--Combat Functions
 function OnEnterCombat(event, creature, victim)
    creature:SendUnitYell("You're no match!", 0)
    creature:RegisterEvent(FlameShock, 1000, 1)
@@ -11,53 +12,54 @@ function OnEnterCombat(event, creature, victim)
    creature:RegisterEvent(BjorisOnHealth, 1000, 0)
 end
 
+--Health check
 function BjorisOnHealth(event, delay, pCall, creature)
-    if (creature:GetHealthPct() <= 75) then
+    if (creature:GetHealthPct() <= 75) then --Check on health
 		creature:RemoveEventById(event)
-		creature:SendUnitYell("You must be a fool to think you will defeat me!", 0)
-		creature:RegisterEvent(FlameShock, 5000, 0)
+		creature:SendUnitYell("You must be a fool to think you will defeat me!", 0) --Yell if health is below 75
+		creature:RegisterEvent(FlameShock, 5000, 0) --Flameshock register enables flameshock
     end
 end
 
 
-function FlameShock(event, delay, pCall, creature)
+function FlameShock(event, delay, pCall, creature) --Flameshock
 	creature:CastSpell(creature:GetVictim(), 29228, true)
 end
 
-function LavaLash(event, delay, pCall, creature)
+function LavaLash(event, delay, pCall, creature) --Lava lash cast
 	creature:CastSpell(creature:GetVictim(), 65974, true)
 end
 
-function HealingTotem(event, delay, pCall, creature)
+function HealingTotem(event, delay, pCall, creature) --Healing totem cast
   creature:CastSpell(creature:GetVictim(), 10463, true)
 end
 
-function StormStrike(event, delay, pCall, creature)
+function StormStrike(event, delay, pCall, creature) --Stormstrike cast
   creature:CastSpell(creature:GetVictim(), 17364, true)
 end
 
-function Windfury(event, delay, pCall, creature)
+function Windfury(event, delay, pCall, creature) --Windfury Buff
   creature:CastSpell(creature:GetVictim(), 58804, true)
 end
 
-function Flametongue(event, delay, pCall, creature)
+function Flametongue(event, delay, pCall, creature) --Flametongue Buff
   creature:CastSpell(creature:GetVictim(), 58789, true)
 end
 
-function OnLeaveCombat(event, creature)
-	creature:RemoveEvents()	
+function OnLeaveCombat(event, creature) --Leave combat check.
+	creature:RemoveEvents()	--Remove combat/dots
     creature:SendUnitYell("Fool!", 0)
-	creature:CastSpell(creature, 23965, true)
+	creature:CastSpell(creature, 23965, true) --Heal NPC to full.
 end
 
-function OnTargetDied(event, creature, victim)
+function OnTargetDied(event, creature, victim) --Player dies
     creature:SendUnitYell("Ha! You can't defeat me, you're no champion!", 0)
 end
 
-function OnDied(event, creature, killer)
-    creature:RemoveEvents()
-    creature:SendUnitYell("You cheated!", 0)
-    creature:DespawnOrUnsummon(5000)
+function OnDied(event, creature, killer) --NPC dies.
+    creature:RemoveEvents() --Removes combat/dots
+    creature:SendUnitYell("You cheated!", 0) --On die yell
+    creature:DespawnOrUnsummon(5000) --Despawn in 5 seconds.
 end
 
 RegisterCreatureEvent(npcId, 1, OnEnterCombat)
--- Hellfire Ramparts/Kain the Demented.lua
+++ Hellfire Ramparts/Kain the Demented.lua
@@ -1,7 +1,7 @@
 local NPC_ID = 600020
 
 function On_Gossip(event, plr, unit)
-
+--Gossip menus, how they appear to the player.
 plr:GossipMenuAddItem(0, "Spawn Level 60-70", 0, 1)
 plr:GossipMenuAddItem(0, "Spawn Level 70-80", 0, 2)
 plr:GossipMenuAddItem(0, "Spawn Level 80-90", 0, 3)
@@ -12,10 +12,10 @@ end
 
 function On_Select(event, player, unit, sender, intid, code)
 
-
-if (intid == 1) then
-unit:SpawnCreature(600022, -1115.922607, 1714.478882, 89.677551, 3.547763)
-unit:Emote(469)
+--Gossip menu ID = function.
+if (intid == 1) then --Option 1
+unit:SpawnCreature(600022, -1115.922607, 1714.478882, 89.677551, 3.547763) --Spawn NPC
+unit:Emote(469) --NPC plays an emote.
 end
 
 
@@ -38,7 +38,7 @@ if (intid == 5) then
 unit:SpawnCreature(600026, -1115.922607, 1714.478882, 89.677551, 3.547763)
 unit:Emote(469)
 end
-	player:GossipComplete()
+	player:GossipComplete() --Closes gossip menu.
 end
 RegisterCreatureGossipEvent(NPC_ID, 1, On_Gossip)
 RegisterCreatureGossipEvent(NPC_ID, 2, On_Select)
